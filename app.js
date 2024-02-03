bbe49f8581c8a4dc735bd913a7540acccbe4071c
Mwakyusa <158697456+Mwakyusa@users.noreply.github.com>
2024-02-03 17:11:19 +0300

    app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const pool = new Pool({
   user: 'mwakyusa',
-  host: 'http://localhost:3000',
+  host: 'localhist',
   database: 'hms',
   password: '2002',
   port: 5432,
